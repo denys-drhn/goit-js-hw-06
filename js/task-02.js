1b71ac553204097d61b17a2f78d37954914149e0
Denys <denys.drhn@gmail.com>
2023-01-31 23:28:19 +0200

    task-2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,18 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+ingredients.forEach(element => {
+	const liElement = document.createElement("li");
+	liElement.textContent = element;
+	liElement.classList.add('item');
+	const list = document.querySelector('#ingredients');
+	list.append(liElement);	
+});
+
+
+
+
+
+
+
