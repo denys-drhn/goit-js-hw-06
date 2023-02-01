30edf9c537f50c9e6031c4b08798fbb3a4ce6985
Denys <denys.drhn@gmail.com>
2023-02-01 22:58:45 +0200

    task-4

---
+++ js/task-04.js
@@ -0,0 +1,24 @@
+const counterValue = {
+	value: 0,
+	increment() {
+		this.value += 1;
+	},
+	
+	decrement() {
+		this.value -= 1;
+	},
+};
+
+const incrementBtn = document.querySelector('button[data-action="increment"]');
+const decrementBtn = document.querySelector('button[data-action="decrement"]');
+const valueEl = document.querySelector('#value');
+
+decrementBtn.addEventListener('click', function () {
+	counterValue.decrement();
+	valueEl.textContent = counterValue.value;
+});
+
+incrementBtn.addEventListener('click', function () {
+	counterValue.increment();
+	valueEl.textContent = counterValue.value;
+});
