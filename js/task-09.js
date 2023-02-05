3ae777c3e3cdcd4f6466c02d3dc85a491764339b
Denys <denys.drhn@gmail.com>
2023-02-05 13:05:13 +0200

    task-9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,18 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const bodyElement = document.querySelector('body')
+
+const changeColorBtn = document.querySelector('button.change-color');
+
+const valueColor = document.querySelector('span.color');
+
+
+changeColorBtn.addEventListener('click', onChangeColorBtnClick);
+
+function onChangeColorBtnClick(event) {
+	bodyElement.style.backgroundColor = getRandomHexColor();
+
+	valueColor.textContent = bodyElement.style.backgroundColor;
+};
+
